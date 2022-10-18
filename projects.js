c8783c10d3273321f455bd6598443aa48aafe3fd
Hari Adhikari <hariadhikari@Kabinas-MacBook-Pro.local>
2022-10-18 15:07:39 +0300

    Runestone and Simba burn bot enabled

--- projects.js
+++ projects.js
@@ -13,6 +13,28 @@ const projects = {
     chatId: -1001715624415,
     buyLink:
       "https://app.uniswap.org/#/swap?inputCurrency=ETH&outputCurrency=0x5a79be6cdce26bc853d72919bf98a0378641b607&use=v2&chain=mainnet"
+  },
+  "0xc68a4c68f17fed266a5e39e7140650acadfe78f8": {
+    name: "Runestone",
+    ticker: "$RUNE",
+    telegram: "https://t.me/runestonetoken",
+    contract: "0xc68a4c68f17fed266a5e39e7140650acadfe78f8",
+    media:
+      "BAACAgQAAx0CcB6ytwADN2NOaW99x-w6PPSRhYcmr7RxLoXvAAJ8DgACh2pwUp__qB24PUTtKgQ",
+    chatId: -1001371904199,
+    buyLink:
+      "https://app.uniswap.org/#/swap?inputCurrency=ETH&outputCurrency=0xc68a4c68f17fed266a5e39e7140650acadfe78f8&use=v2&chain=mainnet"
+  },
+  "0x82c2f739f4af3b401a9f78d9a0d6844db485f098": {
+    name: "Simba Inu",
+    ticker: "$SIMBA",
+    telegram: "https://t.me/kingsimbainu",
+    contract: "0x82c2f739f4af3b401a9f78d9a0d6844db485f098",
+    media:
+      "BAACAgEAAx0CZk4yhwACAgpjTnALPBO1yLwUd86PU7awo3icxAACqgMAAiPIcEYCQuDiaz1tzyoE",
+    chatId: -1001671138082,
+    buyLink:
+      "https://app.uniswap.org/#/swap?inputCurrency=ETH&outputCurrency=0x82c2f739f4af3b401a9f78d9a0d6844db485f098&use=v2&chain=mainnet"
   }
 };
 
