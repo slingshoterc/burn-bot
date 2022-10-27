6fb41eb4e68df3fe908eaeb129e084d11b405e65
brendanwenzel <brendanwenzel@gmail.com>
2022-10-27 14:32:54 +0000

    Update Utils

--- src/utils.js
+++ src/utils.js
@@ -9,25 +9,25 @@ const calcPairAddress = async (token0, token1) => {
       );
 
       const pairAddress = await uniswapV2Factory.getPair(token0, token1)
-
+    return pairAddress;
 }
 
 const calcDollarAmount = async (token, amount) => {
     const IUniV2RouterABI = require("./abi/IUniswapV2Router.json");
       
     const uniswapV2Contract = new ethers.Contract(
-        "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
+        CONTRACTS.UNIV2_ROUTER,
         IUniV2RouterABI,
         fakeWallet
     );
 
     // Find Weth Value
-    const path = [token, "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"]
+    const path = [token, TOKENS.WETH]
     const getAmounts = await uniswapV2Contract.getAmountsOut(amount, path)
     const wethReturned = getAmounts[1]
 
     // Convert Weth to USD
-    const wethUsdcPath = ["0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2", "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"]
+    const wethUsdcPath = [TOKENS.WETH, TOKENS.USDC]
     const getUsdcAmounts = await uniswapV2Contract.getAmountsOut(wethOut, wethUsdcPath)
     const usdcReturned = getUsdcAmounts[1]
 
