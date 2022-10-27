d7606b89b23d96ae2c6ae8110107658f363f25e2
brendanwenzel <brendanwenzel@gmail.com>
2022-10-27 14:30:42 +0000

    Updating Utils

---
+++ src/constants.js
@@ -0,0 +1,21 @@
+const ethers = require("ethers");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
+const fakeWallet = new ethers.Wallet(
+  "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
+  provider
+); 
+
+const TOKENS = {
+    WETH: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+    USDC: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+}
+
+const CONTRACTS = {
+    UNIV2_ROUTER: "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
+    UNIV3_ROUTER: "0x68b3465833fb72a70ecdf485e0e4c7bd8665fc45",
+}
+
+module.exports = { provider, fakeWallet, TOKENS, CONTRACTS }
--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,6 @@
+const { provider, fakeWallet, TOKENS, CONTRACTS } = require("./constants")
+
 const calcPairAddress = async (token0, token1) => {
-    const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
-    const fakeWallet = new ethers.Wallet(
-      "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
-      provider
-    ); 
 
     const uniswapV2Factory = new ethers.Contract(
         "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f",
@@ -16,12 +13,6 @@ const calcPairAddress = async (token0, token1) => {
 }
 
 const calcDollarAmount = async (token, amount) => {
-    const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
-    const fakeWallet = new ethers.Wallet(
-      "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
-      provider
-    ); 
-
     const IUniV2RouterABI = require("./abi/IUniswapV2Router.json");
       
     const uniswapV2Contract = new ethers.Contract(
@@ -34,14 +25,13 @@ const calcDollarAmount = async (token, amount) => {
     const path = [token, "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"]
     const getAmounts = await uniswapV2Contract.getAmountsOut(amount, path)
     const wethReturned = getAmounts[1]
-    logInfo(`Weth: ${wethReturned}`)
 
     // Convert Weth to USD
     const wethUsdcPath = ["0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2", "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"]
     const getUsdcAmounts = await uniswapV2Contract.getAmountsOut(wethOut, wethUsdcPath)
     const usdcReturned = getUsdcAmounts[1]
-    logInfo(`USD: ${usdcReturned}`)
 
+    return usdcReturned;
 };
 
 module.exports = { calcDollarAmount, calcPairAddress }
